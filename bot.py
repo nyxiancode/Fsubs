37e4a24adb0d38cdd9c6157d63d7e03db5626aab
ReaSupport <nekodaisuke.id@gmail.com>
2023-12-09 16:37:33 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -52,7 +52,7 @@ class Bot(Client):
                     f"Silakan periksa kembali var FORCE_SUB_CHANNEL1 dan Pastikan Bot anda Admin di Channel dengan izin link invite Pengguna melalui link undangan, Subs Channel Saat Ini: {FORCE_SUB_CHANNEL1}"
                 )
                 self.LOGGER(__name__).info(
-                    "\nBot Berhenti. Gabung Group https://t.me/pantekyks untuk Bantuan"
+                    "\nBot Berhenti. Gabung Group https://t.me/ReaSupport untuk Bantuan"
                 )
                 sys.exit()
         if FORCE_SUB_CHANNEL2:
@@ -68,7 +68,7 @@ class Bot(Client):
                     f"Silakan periksa kembali var FORCE_SUB_CHANNEL2 dan Pastikan Bot anda Admin di Channel dengan izin link invite Pengguna melalui link undangan, Subs Group Saat Ini: {FORCE_SUB_CHANNEL2}"
                 )
                 self.LOGGER(__name__).info(
-                    "\nBot Berhenti. Gabung Group https://t.me/pantekyks untuk Bantuan"
+                    "\nBot Berhenti. Gabung Group https://t.me/ReaSupport untuk Bantuan"
                 )
                 sys.exit()
         if FORCE_SUB_CHANNEL3:
@@ -97,7 +97,7 @@ class Bot(Client):
                     f"Silakan periksa kembali var FORCE_SUB_GROUP dan Pastikan Bot anda Admin di Channel dengan izin link invite Pengguna melalui link undangan, Subs Group Saat Ini: {FORCE_SUB_CHANNEL2}"
                 )
                 self.LOGGER(__name__).info(
-                    "\nBot Berhenti. Gabung Group https://t.me/pantekyks untuk Bantuan"
+                    "\nBot Berhenti. Gabung Group https://t.me/ReaSupport untuk Bantuan"
                 )
                 sys.exit()
         try:
@@ -111,13 +111,13 @@ class Bot(Client):
                 f"Pastikan Bot adalah Admin di Channel DataBase, dan Periksa kembali Nilai CHANNEL_ID, Nilai Saat Ini: {CHANNEL_ID}"
             )
             self.LOGGER(__name__).info(
-                "\nBot Berhenti. Gabung Group https://t.me/pantekyks untuk Bantuan"
+                "\nBot Berhenti. Gabung Group https://t.me/ReaSupport untuk Bantuan"
             )
             sys.exit()
 
         self.set_parse_mode("html")
         self.LOGGER(__name__).info(
-            f"[🔥 BERHASIL DIAKTIFKAN! 🔥]\n\nBOT Dibuat oleh @{OWNER}\nJika @{OWNER} Membutuhkan Bantuan, Silahkan Tanyakan ke https://t.me/mahadappa"
+            f"[🔥 BERHASIL DIAKTIFKAN! 🔥]\n\nBOT Dibuat oleh @{OWNER}\nJika @{OWNER} Membutuhkan Bantuan, Silahkan Tanyakan ke https://t.me/SayaKyu"
         )
         self.username = usr_bot_me.username
 
