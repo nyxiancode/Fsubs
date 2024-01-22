431d3d4234fd72309edd8a980676917c53cc95a3
ReaSupport <nekodaisuke.id@gmail.com>
2024-01-23 03:53:47 +0700

    Update button.py

--- plugins/button.py
+++ plugins/button.py
@@ -9,15 +9,11 @@ def start_button(client):
     buttons = []
 
     if FORCE_SUB_CHANNEL:
-        buttons.append([InlineKeyboardButton(text="ᴊᴏɪɴ💤", url=client.invitelink)])
+        buttons.append([InlineKeyboardButton(text="ᴊᴏɪɴ💤", url=client.invitelink),
+                        InlineKeyboardButton(text="ᴊᴏɪɴ💤", url=client.invitelink3)])
     if FORCE_SUB_GROUP:
-        buttons.append([InlineKeyboardButton(text="ᴊᴏɪɴ💤", url=client.invitelink2)])
-    if FORCE_SUB_CHANNEL_2:
-        buttons.append([InlineKeyboardButton(text="ᴊᴏɪɴ💤", url=client.invitelink3),
-                        InlineKeyboardButton(text="ᴊᴏɪɴ💤", url=client.invitelink)])
-    if FORCE_SUB_GROUP_2:
-        buttons.append([InlineKeyboardButton(text="ᴊᴏɪɴ💤", url=client.invitelink4),
-                        InlineKeyboardButton(text="ᴊᴏɪɴ💤", url=client.invitelink2)])
+        buttons.append([InlineKeyboardButton(text="ᴊᴏɪɴ💤", url=client.invitelink2),
+                        InlineKeyboardButton(text="ᴊᴏɪɴ💤", url=client.invitelink4)])
 
     buttons.extend([
         [InlineKeyboardButton(text="ʜᴇʟᴘ & ᴄᴏᴍᴍᴀɴᴅs", callback_data="help")],
