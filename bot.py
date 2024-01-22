2e1cd7fb2e94034ed6ab125bd9e233484cb98b90
ReaSupport <nekodaisuke.id@gmail.com>
2024-01-22 23:37:42 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,9 +3,9 @@
 # Kalo clone Gak usah hapus 
 # gue tandain akun tele nya ngentod
 
-import os
+import pyromod.listen
 import sys
-import asyncio
+
 from pyrogram import Client
 
 from config import (
@@ -37,13 +37,6 @@ class Bot(Client):
 
     async def start(self):
         await super().start()
-
-        # Menyesuaikan zona waktu
-        os.environ["TZ"] = "UTC"
-        sys.stdout.write('Setting timezone to UTC\n')
-        sys.stdout.flush()
-        await asyncio.sleep(2)  # Tunggu sejenak agar perubahan waktu berlaku
-
         usr_bot_me = await self.get_me()
 
         if FORCE_SUB_CHANNEL1:
@@ -59,7 +52,7 @@ class Bot(Client):
                     f"Silakan periksa kembali var FORCE_SUB_CHANNEL1 dan Pastikan Bot anda Admin di Channel dengan izin link invite Pengguna melalui link undangan, Subs Channel Saat Ini: {FORCE_SUB_CHANNEL1}"
                 )
                 self.LOGGER(__name__).info(
-                    "\nBot Berhenti. Gabung Group https://t.me/ReaSupport untuk Bantuan"
+                    "\nBot Berhenti. Gabung Group https://t.me/pantekyks untuk Bantuan"
                 )
                 sys.exit()
         if FORCE_SUB_CHANNEL2:
@@ -75,7 +68,7 @@ class Bot(Client):
                     f"Silakan periksa kembali var FORCE_SUB_CHANNEL2 dan Pastikan Bot anda Admin di Channel dengan izin link invite Pengguna melalui link undangan, Subs Group Saat Ini: {FORCE_SUB_CHANNEL2}"
                 )
                 self.LOGGER(__name__).info(
-                    "\nBot Berhenti. Gabung Group https://t.me/ReaSupport untuk Bantuan"
+                    "\nBot Berhenti. Gabung Group https://t.me/pantekyks untuk Bantuan"
                 )
                 sys.exit()
         if FORCE_SUB_CHANNEL3:
@@ -104,7 +97,7 @@ class Bot(Client):
                     f"Silakan periksa kembali var FORCE_SUB_GROUP dan Pastikan Bot anda Admin di Channel dengan izin link invite Pengguna melalui link undangan, Subs Group Saat Ini: {FORCE_SUB_CHANNEL2}"
                 )
                 self.LOGGER(__name__).info(
-                    "\nBot Berhenti. Gabung Group https://t.me/ReaSupport untuk Bantuan"
+                    "\nBot Berhenti. Gabung Group https://t.me/pantekyks untuk Bantuan"
                 )
                 sys.exit()
         try:
@@ -118,13 +111,13 @@ class Bot(Client):
                 f"Pastikan Bot adalah Admin di Channel DataBase, dan Periksa kembali Nilai CHANNEL_ID, Nilai Saat Ini: {CHANNEL_ID}"
             )
             self.LOGGER(__name__).info(
-                "\nBot Berhenti. Gabung Group https://t.me/ReaSupport untuk Bantuan"
+                "\nBot Berhenti. Gabung Group https://t.me/pantekyks untuk Bantuan"
             )
             sys.exit()
 
         self.set_parse_mode("html")
         self.LOGGER(__name__).info(
-            f"[🔥 BERHASIL DIAKTIFKAN! 🔥]\n\nBOT Dibuat oleh @{OWNER}\nJika @{OWNER} Membutuhkan Bantuan, Silahkan Tanyakan ke https://t.me/SayaKyu"
+            f"[🔥 BERHASIL DIAKTIFKAN! 🔥]\n\nBOT Dibuat oleh @{OWNER}\nJika @{OWNER} Membutuhkan Bantuan, Silahkan Tanyakan ke https://t.me/mahadappa"
         )
         self.username = usr_bot_me.username
 
